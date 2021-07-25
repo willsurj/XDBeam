a0702a14edc0d5b87fb63915f712edd74fc17921
Will <willsurj0@gmail.com>
2021-07-25 16:53:59 +1000

    📩 Moved Wallet into new component

--- sender-svelte-app/src/App.svelte
+++ sender-svelte-app/src/App.svelte
@@ -1,48 +1,8 @@
 <script>
-	import {
-		apiData,
-		apiLoading
-	} from './store';
-
-	import {
-		onMount
-	} from 'svelte';
-
-	import Button from './components/Button.svelte';
+	import Wallet from './components/Wallet.svelte';
 	import Header from './components/Header.svelte';
-
-	const apiRequest = async () => {
-		apiLoading.set(true);
-		fetch("https://frontier.testnet.digitalbits.io/assets")
-			.then(response => response.json())
-			.then(data => {
-				console.log(data);
-				apiData.set(data);
-				apiLoading.set(false);
-			}).catch(error => {
-				console.log(error);
-				return {};
-			})
-
-	}
-
-	let btnText;
-
-	$: if ($apiLoading) {
-		btnText = "Loading..."
-	} else {
-		btnText = "Pull from API";
-	}
-
-	onMount(apiRequest);
 </script>
 
 <Header />
 
-<main>
-	<Button on:click="{apiRequest}">{btnText}</Button>
-
-	<p>
-		{JSON.stringify($apiData)}
-	</p>
-</main>
+<Wallet />
---
+++ sender-svelte-app/src/components/Wallet.svelte
@@ -0,0 +1,45 @@
+<script>
+    import {
+        apiData,
+        apiLoading
+    } from '../store';
+
+    import {
+        onMount
+    } from 'svelte';
+
+    import Button from './Button.svelte'
+
+    const apiRequest = async () => {
+        apiLoading.set(true);
+        fetch("https://frontier.testnet.digitalbits.io/assets")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                apiData.set(data);
+                apiLoading.set(false);
+            }).catch(error => {
+                console.log(error);
+                apiLoading.set(false);
+                return {};
+            })
+    }
+
+    let btnText;
+
+    $: if ($apiLoading) {
+        btnText = "Loading..."
+    } else {
+        btnText = "Pull from API";
+    }
+
+    onMount(apiRequest);
+</script>
+
+<main>
+    <Button on:click="{apiRequest}">{btnText}</Button>
+
+    <p>
+        {JSON.stringify($apiData)}
+    </p>
+</main>
